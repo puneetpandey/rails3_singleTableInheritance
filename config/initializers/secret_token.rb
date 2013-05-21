d646ef5f5f238a78b1608919a2677cacf89030c7
unknown <puneet.pandey@del1-dhp-28291.synapse.com>
2013-05-21 16:07:34 +0530

    First commit for Rails 3 STI app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3Singletableinheritance::Application.config.secret_token = 'bef919318d6bbdc008f7f85cfd32616baca94baacb71a2560f0c824594d3af46d5637dc6d710eee965193b323604bf5a78f5fe088eace2979ac1b9c9a2ebfa7c'
